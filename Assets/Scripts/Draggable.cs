b17344803a5396f5f5a823873e7352b3bd2f9873
dthoma6403 <dthoma6403@gmail.com>
2016-10-21 20:28:40 -0700

    Set up Canvas and draggabale Script

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public void OnBeginDrag(PointerEventData eventData){
+		Debug.Log ("DRAGGING");
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		Debug.Log ("DRAGGING");
+
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		Debug.Log ("DRAGGING");
+	}
+}
