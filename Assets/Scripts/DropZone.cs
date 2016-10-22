9d252566dbedaccb60a1f9bfb940fe46b31a7dae
dthoma6403 <dthoma6403@gmail.com>
2016-10-21 22:16:43 -0700

    Beginning a Drop Zone Implementation

---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler{
+
+	public void OnPointerEnter(PointerEventData eventData){
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		
+	}
+
+	public void OnDrop(PointerEventData eventData){
+		
+	}
+
+
+}
