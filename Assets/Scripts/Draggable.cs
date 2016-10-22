d924e5f3262135d1aeeb4ecd48a15f460a860e4b
dthoma6403 <dthoma6403@gmail.com>
2016-10-21 21:30:38 -0700

    parentToReturnTo

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -3,8 +3,13 @@ using System.Collections;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	Transform parentToReturnTo = null;
+
 	public void OnBeginDrag(PointerEventData eventData){
 		Debug.Log ("Beginning to Drag");
+
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent (this.transform.parent.parent);
 	}
 
 	public void OnDrag(PointerEventData eventData){
@@ -15,5 +20,6 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
 	public void OnEndDrag(PointerEventData eventData){
 		Debug.Log ("Stopped Dragging");
+		this.transform.SetParent (parentToReturnTo);
 	}
 }
